dfbd812d9c2e7576e632c4740a0fbea91a58c628
Adeleye Tolulope <tolulope@grit.systems>
2020-01-28 09:45:03 +0100

    Updates

---
+++ precache-manifest.bfa9b8b157a8ec9a1e92559ee3baef58.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143ad115a7a2826c5b554ffdcbdd8602",
+    "url": "/Motadeh/pet-shop/index.html"
+  },
+  {
+    "revision": "2a33ab9e054ae0c9c044",
+    "url": "/Motadeh/pet-shop/static/css/main.36e4140d.chunk.css"
+  },
+  {
+    "revision": "0a223cdaed4002d9e2f4",
+    "url": "/Motadeh/pet-shop/static/js/2.a9deaa4b.chunk.js"
+  },
+  {
+    "revision": "2a33ab9e054ae0c9c044",
+    "url": "/Motadeh/pet-shop/static/js/main.b48872f4.chunk.js"
+  },
+  {
+    "revision": "038afe7b1da441009349",
+    "url": "/Motadeh/pet-shop/static/js/runtime~main.5a2809e0.js"
+  },
+  {
+    "revision": "2bf4f65ea58f7723da714750d68fd040",
+    "url": "/Motadeh/pet-shop/static/media/app-iPhone.2bf4f65e.png"
+  },
+  {
+    "revision": "210acfb338f59f02d54c76a7b8f9ecb8",
+    "url": "/Motadeh/pet-shop/static/media/dogs.210acfb3.jpg"
+  },
+  {
+    "revision": "d0558d91063038236b60e3ef71fdc1fd",
+    "url": "/Motadeh/pet-shop/static/media/download-app.d0558d91.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Motadeh/pet-shop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2536c1d875b8345ede465c3d293a364e",
+    "url": "/Motadeh/pet-shop/static/media/puppy-and-kitten-animal-wallpaper-hd-dog-photos-puppies-pets-lovely-widescreen-windows-wallpapers-of-dogs-mac-1805x1015.2536c1d8.jpeg"
+  },
+  {
+    "revision": "076415946fd273adc902763ea7dbf954",
+    "url": "/Motadeh/pet-shop/static/media/sitting.07641594.jpeg"
+  }
+]);
